1f634ff6f5048b6eff9d86f0f927a3d68d901d77
Swarup Dhar <swarupdhar123@gmail.com>
2024-01-14 12:29:47 -0600

    asldjf

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 set -e
 
 pacman -S --needed \
-	git \
+    git \
 	firefox \
 	neovim \
 	nemo \
@@ -21,7 +21,10 @@ pacman -S --needed \
 	ttf-iosevkaterm-nerd \
 	ttf-noto-nerd \
     noto-fonts-emoji \
-	pavucontrol
+	pavucontrol \
+    qt5-graphicaleffects \
+    qt5-quickcontrols2 \
+    qt5-svg \
 
 # download rose pine theme
 wget https://github.com/rose-pine/gtk/releases/download/v2.1.0/gtk3.tar.gz -P /tmp
@@ -29,16 +32,22 @@ wget https://github.com/rose-pine/gtk/releases/download/v2.1.0/gtk3.tar.gz -P /t
 wget https://github.com/rose-pine/gtk/releases/download/v2.1.0/rose-pine-icons.tar.gz -P /tmp
 
 # unzip into themes and icon folders
-mkdir -p "$HOME/.themes"
-mkdir -p "$HOME/.icons"
+mkdir -p $HOME/.themes
+mkdir -p $HOME/.icons
 tar -xf /tmp/gtk3.tar.gz -C /tmp
 #tar -xf /tmp/gtk4.tar.gz -C /tmp
 tar -xf /tmp/rose-pine-icons.tar.gz -C /tmp
-cp -r /tmp/gtk3/rose-pine-gtk "$HOME/.themes/rose-pine-gtk"
-cp -r /tmp/icons/rose-pine-icons "$HOME/.icons/rose-pine-icons"
+cp -r /tmp/gtk3/rose-pine-gtk $HOME/.themes/rose-pine-gtk
+cp -r /tmp/icons/rose-pine-icons $HOME/.icons/rose-pine-icons
 
 # set gsettings
 gsettings set org.gnome.desktop.interface gtk-theme rose-pine-gtk
 gsettings set org.gnome.desktop.interface icon-theme rose-pine-gtk
 gsettings set org.gnome.desktop.wm.preferences theme rose-pine-gtk
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+
+# set sddm theme
+git clone https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme
+cp /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
+echo "[Theme]
+Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf
